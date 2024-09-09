30ca2049aefa0b921a2ce047cddbddf9592ad4cb
Shiv Patel <shivpatel0204@gmail.com>
2024-09-08 22:14:14 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -6,3 +6,23 @@ function calculateAverageSales(sales)
     const averageSales = totalSales / sales.length;// Finding the Avg.
         return averageSales;
 }
+
+
+//Task 2: Function to Determine Performance Rating
+
+function determinePerformanceRating(averageSales)
+{
+    if (averageSales > 10000) {
+        return "Excellent"; // Returns Excellent if Sales are above $10,000
+                              }
+    else if (averageSales >= 7000 && averageSales <= 10000) {
+        return "Good"; //Returns Good if Sales btwn $10,000 and $7,000
+         }         
+    else if (averageSales >= 4000 && averageSales <= 7000) {
+        return "Satisfactory"; //Returns Satisfactory if Sales btwn $7,000 and $4,000 
+         } 
+     else {
+        return "Need Improvement"; //Returns Needs Improvement if Sales lower than $4,000 
+         }
+}
+
